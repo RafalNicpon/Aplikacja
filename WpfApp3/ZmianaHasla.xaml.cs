69481a32cd4b7b85fac2dc2f25a43806bc8f720b
Rafal <rafal.nicpon1@gmail.com>
2023-06-14 22:41:46 +0200

    Zmiany

---
+++ WpfApp3/ZmianaHasla.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public partial class ZmianaHasla : Window
+    {
+        public ZmianaHasla()
+        {
+            InitializeComponent();
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage main = new MainPage();
+            main.Show();
+            Close();
+        }
+        private void Cofnij_Click(object sender, RoutedEventArgs e)
+        {
+            Setting setting = new Setting();
+            setting.Show();
+            Close();
+        }
+        private void Zmień_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            MessageBox.Show("Twoje hasło zostało zmienione!", "Zmiana hasła", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainPage main = new MainPage();
+            main.Show();
+            Close();
+        }
+
+    }
+}
---
+++ WpfApp3/ZmianaTelefonu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ZmianaTelefonu.xaml
+    /// </summary>
+    public partial class ZmianaTelefonu : Window
+    {
+        public ZmianaTelefonu()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage main = new MainPage();
+            main.Show();
+            Close();
+        }
+
+        private void Numer_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Twój numer został zmieniony!", "Zmiana numeru telefonu", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainPage main = new MainPage();
+            main.Show();
+            Close();
+        }
+
+        private void Cofnij_Click(object sender, RoutedEventArgs e)
+        {
+            Setting setting = new Setting();
+            setting.Show();
+            Close();
+        }
+    }
+}
